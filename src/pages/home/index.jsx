c57d5ed883ce05a0d4f96db3c3edae33b28e700b
meryem-uludagli <meryemuslu2035@gmail.com>
2025-01-20 12:03:19 +0200

    update

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import Hero from "../../components/hero";
 import List from "../../components/list";
+import CartButton from "../../components/buttons/cart-button";
+import TrendButton from "../../components/buttons/trend-button";
 
 const Home = () => {
   return (
-    <div>
+    <div className="relative">
       <Hero />
+
+      <CartButton />
+      <TrendButton />
+
       <List />
     </div>
   );
